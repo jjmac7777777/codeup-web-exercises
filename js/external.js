45bd775d457ce2d613c561a55c06f46bb229fb88
johnmccloskey <john.j.mccloskey76@gmail.com>
2021-05-27 11:15:16 -0500

    added required exercises

---
+++ js/external.js
@@ -0,0 +1,21 @@
+function shout(message) {
+    alert(message.toUpperCase((Welcome to my Website!) + "!!!");
+}
+
+        var returnValue = shout('Welcome to my Website!');
+        console.log(returnValue);
+
+var color = favoriteColor;
+    prompt('Whats your favorite color?');
+    return color (blue);
+    function shout (message) {
+        alert(message.toUpperCase("Great, blue is my favorite color too!");
+    }
+        var returnValue = shout("Great, blue is my favorite color too!");
+        console.log(returnValue);
+
+function increment(someNumber) {
+    return someNumber + 1;
+    console.log('Which nunmber?');
+    return someNumber + 2;
+}
