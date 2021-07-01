7b2673619c53411e5fad7b835483ebdbd512213d
johnmccloskey <john.j.mccloskey76@gmail.com>
2021-07-01 09:31:45 -0500

    Morning Warmup 7-1-21

---
+++ js/morningWarmUps.js
@@ -0,0 +1,20 @@
+// TODO: Create a function named countSpaces that accepts a string and
+//  returns the total number of spaces in the string.
+
+function countSpaces(str) {
+    if typeof str !== 'string'{
+        return false;
+    }
+    else {
+        return count countSpaces(total number of Spaces in the string);
+    }
+
+    let my_string = "total number of Spaces in the string";
+    let spaceCount = (my_string.split(" ").length - 1);
+    console.log(spaceCount);
+
+}
+
+
+
+// TODO: Add validation to function above, if the argument pass is not a string it should return false.
