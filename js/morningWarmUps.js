0504691e1b2eb7e557ec9167352a7e5536ac488f
johnmccloskey <john.j.mccloskey76@gmail.com>
2021-07-08 17:54:04 -0500

    7-8-21

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -49,38 +49,71 @@
 // TODO: Create a method on the object down below named getFullName
 //  that when called returns the users full name using the "this" key term.
 // Example: personOne.getFullName() ---- returns ----> "silvia floopertan"
-var personOne = {
-    firstName: "silvia",
-    lastName: "floopertan",
-    ageInYears: 34,
-    heightInInches: 65
+// var personOne = {
+//     firstName: "silvia",
+//     lastName: "floopertan",
+//     ageInYears: 34,
+//     heightInInches: 65,
+//     getFullName : function() {
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
+//
+// personOne`${person} is ${age} years old`;
+//
+// // TODO: Create a method on the object down below named getNicelyFormattedFullName that when called returns the
+// //  users full name with both first letters in the names being capitalized look at example down below for more reference.
+// // Example: personOne.getNicelyFormattedFullName() ---- returns ----> "Dan Valdarez"
+// var personTwo = {
+//     firstName: "dan",
+//     lastName: "valdarez",
+//     ageInYears: 61,
+//     heightInInches: 73
+// }
+//
+//
+// // TODO: Create a method on the object below that gives us the behavior that the example implies.
+// // Example: personThree.intro() ---- returns ----> "Hello, My name is Arlene Martinez and I am 25 years old"
+// var personThree = {
+//     firstName: "arlene",
+//     lastName: "martinez",
+//     ageInYears: 25,
+//     heightInInches: 62
+// }
 
-    fullName : function(getFullName) {
-        return this.firstName + " " + this.lastName;
-    }
-};
 
-personOne`${person} is ${age} years old`;
+// July 8th 2021
+// Part 1
+// TODO: Create a function that takes an array of strings and returns an array of objects
+//  where each object has 2 properties: originalString and countOfLetters
+// Example: input: ["hello", "dave"]
+//          returns: [
+//                    { "originalString":"hello", lengthOfOriginalString:5 },
+//                    { "originalString":"dave", lengthOfOriginalString:4 }
+//                   ]
 
-// TODO: Create a method on the object down below named getNicelyFormattedFullName that when called returns the
-//  users full name with both first letters in the names being capitalized look at example down below for more reference.
-// Example: personOne.getNicelyFormattedFullName() ---- returns ----> "Dan Valdarez"
-var personTwo = {
-    firstName: "dan",
-    lastName: "valdarez",
-    ageInYears: 61,
-    heightInInches: 73
+let array = ["Hello" , "Dave"];
+
+function returnSplitString(string){
+    return string.split(" ");
 }
 
-
-// TODO: Create a method on the object below that gives us the behavior that the example implies.
-// Example: personThree.intro() ---- returns ----> "Hello, My name is Arlene Martinez and I am 25 years old"
-var personThree = {
-    firstName: "arlene",
-    lastName: "martinez",
-    ageInYears: 25,
-    heightInInches: 62
-}
+let splitStringArray = returnSplitString(array[0]);
 
 
+// Part 2
+// TODO: Create a function that takes the array of objects from PART 1
+//  and returns a string of all of the originalStrings concatenated with spaces in between
+// Example: input: [
+//                    { "originalString":"hello", lengthOfOriginalString:5 },
+//                    { "originalString":"dave", lengthOfOriginalString:4 }
+//                  ]
+//          returns: "hello dave"
+// TODO: Create a function named getTallUsers that accepts an array of objects. The objects in the array
+//  will be in the same format as PersonOne, PersonTwo, and PersonThree. The functions job is to return
+//  an array of users that have heightInInches equal to or greater than 65.
+//var people = [personOne,personTwo,personThree];
+// Example: getTallUsers(people)
+// ---- returns ----> [{firstName: "silvia", lastName: "floopertan", ageInYears: 34, heightInInches: 65},
+//
 
