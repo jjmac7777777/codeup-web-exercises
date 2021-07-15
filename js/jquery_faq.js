3b2ecb9f2eebb02626d4d87e78184ed805382881
johnmccloskey <john.j.mccloskey76@gmail.com>
2021-07-15 16:58:15 -0500

    MapBox Exercise 7-15-21

--- js/jquery_faq.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-$(document).ready(function() {
-    alert("Source: https://www.nps.gov/aboutus/faqs.htm");
-    $('dd').addClass('invisible')
-    $('#button').click(function(event) {
-        event.preventDefault();
-        $('dd').toggleClass('invisible');
-    });
-});
